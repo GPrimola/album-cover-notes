5745e345d92bc2af858d9fa09653ecbcf59d72c7
torres.giorgio@gmail.com <torres.giorgio@gmail.com@d18a8c21-d1e3-3c3f-2a77-cd4083adff7b>
2011-08-18 12:17:24 +0000

    comentários sobre autor.

--- trunk/AlbumCoverNotes/src/com/exemplo/main/Row.java
+++ trunk/AlbumCoverNotes/src/com/exemplo/main/Row.java
@@ -1,3 +1,19 @@
+
+/*
+ * Por favor, ao usar, divulgar ou modificar, fazer referência ao material do autor.
+ * 
+ * Please, you can use, share or edit, but make reference to the author's material.
+ * 
+ * Je vous en prie, vous pouvez utiliser, partager et modifier, mais faites reference au materiel d'auteur.
+ * 
+ * Disponível/Available/Disponible em/in/à: 
+ *     
+ *         SVN: http://album-cover-notes.googlecode.com/svn/trunk/
+ *
+ * 
+ * @author Giorgio P. F. G. Torres
+ */
+
 package com.exemplo.main;
 
 import android.graphics.Bitmap;
